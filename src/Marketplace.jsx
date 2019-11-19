62e6cfc4a1ea40301b7d6aa32b7ebce331284386
AmandaBurstein <amburst@gmail.com>
2019-11-19 12:10:46 -0500

    new all-items endpoint

--- src/Marketplace.jsx
+++ src/Marketplace.jsx
@@ -4,14 +4,24 @@ import { Link } from "react-router-dom";
 class Marketplace extends Component {
   render() {
     return (
-      <div className="marketplace">
-        <div>
-          <Link to={"/itemdetails" + this.props.id}>
-            <img src={this.props.frontendPath} height="100px" />
+      <div>
+        <div className="navbar-page">
+          <div className="store-mini">Jasallanda Sweet Market</div>
+          <Link className="link" to="/">
+            HOME
           </Link>
-          <div>Description</div>
-          <div>Price</div>
-          <Link to={"/seller/" + this.props.sellerId}>Seller information</Link>
+        </div>
+        <div className="marketplace">
+          <div>
+            <Link to={"/itemdetails" + this.props.id}>
+              <img src={this.props.frontendPath} height="100px" />
+            </Link>
+            <div>Description</div>
+            <div>Price</div>
+            <Link to={"/seller/" + this.props.sellerId}>
+              Seller information
+            </Link>
+          </div>
         </div>
       </div>
     );
