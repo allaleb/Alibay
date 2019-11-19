7af85051068be42952e1df33f2964255d7c8d193
Jason Eng <jason.eng222@gmail.com>
2019-11-19 16:08:02 -0500

    Initial itemreviews file

---
+++ src/ItemReviews.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+class ItemReviews extends Component {
+  constructor() {
+    super();
+    this.state = {
+      reviewer: "",
+      itemBeingReviewed: "",
+      review: ""
+    };
+  }
+  reviewChangeHandler = event => {
+    this.setState({ review: event.target.value });
+  };
+  submitHandler = async event => {
+    event.preventDefault();
+    let data = new FormData();
+    data.append("review", this.state.review);
+    data.append("reviewer", this.state.reviewer);
+    data.append("itembeingreviewed", this.state.itemBeingReviewed);
+    let response = await fetch("/upload-review", {
+      method: "POST",
+      body: data
+    });
+    let body = await response.json();
+  };
+  render = () => {
+    return (
+      <form onSubmit={this.submitHandler}>
+        <input type="text" onChange={this.reviewChangeHandler}></input>
+      </form>
+    );
+  };
+}
+export default ItemReviews;
