2ad982ee4a71821c46e33a52f547ad8a63803e00
Alla Lebedeva <allalebedeva@air-de-alla.wireless.concordia.ca>
2019-11-20 14:11:36 -0500

    Cart was modified!

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -1,23 +1,23 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
-
+import { stringify } from "querystring";
 
 class UnconnectedCart extends Component {
   render = () => {
     return (
       <div>
+        <ul>
+          {this.props.cart.map(item => {
+            return (
+              <li className="itemsInCart">
+                <img src={item.frontendPath} height="100px" />
+                {item.name + " " + "$" + item.price}
+              </li>
+            );
+          })}
+        </ul>
         <button>Checkout</button>
-
-        {/* <Link to="/">Homepage</Link>
-        <h3>Your Cart</h3>
-        <h4>Total</h4>
-        <button>Checkout</button>
-        <div className="navbar">
-          <Link className="link" to="/">
-            HOME
-          </Link>
-        </div> */}
       </div>
     );
   };
