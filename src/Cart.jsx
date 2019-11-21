77591afe3620e9255eb136b5e6a93004440d4474
Jason Eng <jason.eng222@gmail.com>
2019-11-21 13:56:04 -0500

    added navbar to cart

--- src/Cart.jsx
+++ src/Cart.jsx
@@ -6,17 +6,38 @@ class UnconnectedCart extends Component {
   render = () => {
     return (
       <div>
-        <ul>
-          {this.props.cart.map(item => {
-            return (
-              <li className="itemsInCart">
-                <img src={item.frontendPath} height="100px" />
-                {item.name + " " + "$" + item.price}
-              </li>
-            );
-          })}
-        </ul>
-        <button>Checkout</button>
+        <div className="navbar-page">
+          <div className="store-mini">
+            <Link className="link" to="/">
+              Jasallanda Sweet Market
+            </Link>
+          </div>
+          <Link className="link" to="/marketplace">
+            Marketplace
+          </Link>
+          <Link className="link" to="/myprofile">
+            My Profile
+          </Link>
+          <Link className="link" to="/" onClick={this.logOutHandler}>
+            Log Out
+          </Link>
+          <Link className="link" to="/">
+            Home
+          </Link>
+        </div>
+        <div>
+          <ul>
+            {this.props.cart.map(item => {
+              return (
+                <li className="itemsInCart">
+                  <img src={item.frontendPath} height="100px" />
+                  {item.name + " " + "$" + item.price}
+                </li>
+              );
+            })}
+          </ul>
+          <button>Checkout</button>
+        </div>
       </div>
     );
   };
